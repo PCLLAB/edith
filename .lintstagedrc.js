ffc018629124ddcfeebca7432d9ef97c35c40c7c
Kyle Zheng <kylezheng73@gmail.com>
2022-04-30 06:13:45 -0400

    chore: precommit hook

---
+++ .lintstagedrc.js
@@ -0,0 +1,12 @@
+// Needed for to use lint-staged with nextjs
+
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
+
+module.exports = {
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+};
